2409255ace11667952ab420f615760d430171c4a
derick <dj8654974@gmail.com>
2025-09-22 13:12:12 +0300

    client update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,11 +14,14 @@ export default function Register() {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(form),
-      });
+      const res = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/auth/register`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(form),
+        }
+      );
 
       if (!res.ok) {
         const msg = await res.json();
